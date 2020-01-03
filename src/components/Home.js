815897c59b4ee272e35530f98bfa9de911435dd7
= <parshvashah414@gmail.com>
2020-01-03 18:31:03 -0500

    Pushed ref

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 class Home extends React.Component {
   render() {
-    return <Container></Container>;
+    return (
+      <Container>
+        <Row>
+          <Col>1 of 2</Col>
+          <Col>2 of 2</Col>
+        </Row>
+      </Container>
+    );
   }
 }
 export default Home;
