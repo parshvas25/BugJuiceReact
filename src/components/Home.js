d86b68b9c4744bfd2f690e5d00d719b02ec3877b
= <parshvashah414@gmail.com>
2020-01-03 19:36:19 -0500

    Add excelsiors affiliate

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,15 +2,23 @@ import React from 'react';
 import Container from '@material-ui/core/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import '../styles/home.css';
+import excelsiors from '../images/excelsiors.png';
 
 class Home extends React.Component {
   render() {
     return (
       <Container>
-        <Row>
-          <Col>1 of 2</Col>
-          <Col>2 of 2</Col>
-        </Row>
+        <div className='background'>
+          <Row>
+            <Col>
+              <div className='affiliates'>
+                <h2>Assosiated Affiliates </h2>
+                <img src={excelsiors}></img>
+              </div>
+            </Col>
+          </Row>
+        </div>
       </Container>
     );
   }
