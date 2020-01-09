4aa7036103a1f2831add23b114768fd6e780e9dc
= <parshvashah414@gmail.com>
2020-01-09 10:04:31 -0500

    Add components

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,9 +2,11 @@ import React from 'react';
 import Container from '@material-ui/core/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Carousel from 'react-bootstrap/Carousel';
 import '../styles/home.css';
 import excelsiors from '../images/excelsiors.png';
 import north_stars from '../images/northstars.png';
+import bjuice1 from '../images/bjuice1.png';
 
 class Home extends React.Component {
   render() {
@@ -12,7 +14,32 @@ class Home extends React.Component {
       <Container>
         <div className='background'>
           <Row>
-            <Col></Col>
+            <Col>
+              <h2>Add Font Awesome Here</h2>
+              <Carousel>
+                <Carousel.Item>
+                  <img
+                    className='d-block w-100'
+                    src={bjuice1}
+                    alt='First slide'
+                  />
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img
+                    className='d-block w-100'
+                    src={bjuice1}
+                    alt='Second slide'
+                  />
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img
+                    className='d-block w-100'
+                    src={bjuice1}
+                    alt='Third slide'
+                  />
+                </Carousel.Item>
+              </Carousel>
+            </Col>
             <Col></Col>
             <Col>
               <div className='affiliates'>
