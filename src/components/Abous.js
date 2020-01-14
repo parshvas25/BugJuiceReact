9df43e0f4ce8401e1b944baa7491b2950037b0b9
= <parshvashah414@gmail.com>
2020-01-14 16:40:43 -0500

    Pushed refs

---
+++ src/components/Abous.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Carousel from 'react-bootstrap/Carousel';
+import '../styles/home.css';
+import excelsiors from '../images/excelsiors.png';
+import north_stars from '../images/northstars.png';
+import bjuice1 from '../images/bjuice1.png';
